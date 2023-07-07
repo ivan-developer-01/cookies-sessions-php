387a21650ef23b61824f1aa781a7617941cd4dec
Ivan <iw.kolosov2016@yandex.ru>
2023-07-07 17:18:55 +0300

    added the getUsersList function code

--- _functions.php
+++ _functions.php
@@ -1,6 +1,30 @@
 <?php
+$USERS_DB = '_users.db.txt';
+$USER_DELIMITER = ':';
+
 function getUsersList() {
-	// ???
+	global $USERS_DB;
+	global $USER_DELIMITER;
+
+	$usersDbFile = fopen($USERS_DB, 'r');
+	$usersList = array();
+
+	while (!feof($usersDbFile)) {
+		$line = trim(fgets($usersDbFile));
+		$explodedLine = explode($USER_DELIMITER, $line);
+		$login = $explodedLine[0];
+		$passwordHash = $explodedLine[1];
+
+		$user = array(
+			'login' => $login,
+			'password' => $passwordHash
+		);
+
+		$usersList[] = $user;
+	}
+
+	fclose($usersDbFile);
+	return $usersList;
 }
 
 function existsUser() {
